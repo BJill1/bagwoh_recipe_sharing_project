c56b322520f8e6b8d0cc6330a7178c0b338c347e
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-10-17 10:46:35 +0000

    UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
 import { CreateRecipe } from './components/CreateRecipe.jsx'
+import { RecipeFilter } from './components/RecipeFilter.jsx'
+import { RecipeSorting } from './components/RecipeSorting.jsx'
+import { RecipeList } from './components/RecipeList.jsx'
+const recipes = [
+  {
+    title: 'Boiled egg',
+    author: "Jill Bagwoh",
+    ingredients: ['water', 'egg'],
+    steps: ['Put some egg in water', 'Boil for 0 minutes']
+  },
+  { 
+    title: 'Boiled rice',
+    author: "Iliasu Ndimo",
+    ingredients: ['water', 'rice'],
+    steps: ['Put some egg in water', 'Boil for 30 minutes']
+   },
+]
 export function App() {
-  return <CreateRecipe />
+  return (
+    <div style={{ padding: 8 }}>
+      <CreateRecipe />
+      <br />
+      <hr />
+      Filter by:
+      <RecipeFilter field='author' />
+      <br />
+      <RecipeSorting fields={['createdAt', 'updatedAt']} />
+      <hr />
+      <RecipeList recipes={recipes} />
+    </div>
+  )
 }
 
     
---
+++ src/components/RecipeFilter.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+export function RecipeFilter({ field }) {
+  return (
+    <div>
+      <label htmlFor={`filter-${field}`}>{field}: </label>
+      <input type='text' name={`filter-${field}`} id={`filter-${field}`} />
+    </div>
+  )
+}
+RecipeFilter.propTypes = {
+  field: PropTypes.string.isRequired,
+}
