8cbe582b0a0a21e30e909ac3eab944bb819298b2
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-12-09 10:53:00 +0000

    authentication to join rooms

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
 import { Login } from './pages/Login.jsx'
 import { io } from 'socket.io-client'
-const socket = io(import.meta.env.VITE_SOCKET_HOST)
+const socket = io(import.meta.env.VITE_SOCKET_HOST, {
+  query: 'room=' + new URLSearchParams(window.location.search).get('room'),
+  auth: {
+    token: new URLSearchParams(window.location.search).get('token'),
+  },
+
+})
 const queryClient = new QueryClient()
 const router = createBrowserRouter([
   {
@@ -21,9 +27,11 @@ const router = createBrowserRouter([
     element: <Login />,
   },
 ])
-socket.on('connect', () => {
+socket.on('connect', async () => {
   console.log('connected to socket.io as', socket.id)
-  socket.emit('chat.message', 'hello from client')
+  socket.emit('chat.message', new URLSearchParams(window.location.search).get('mymsg'),)
+  const userInfo = await socket.emitWithAck('user.info', socket.id)
+  console.log('user info', userInfo)
 })
 socket.on('connect_error', (err) => {
   console.error('socket.io connect error:', err)
